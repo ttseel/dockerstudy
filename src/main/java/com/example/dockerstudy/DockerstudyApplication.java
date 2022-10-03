5008572bfbc4de0b6673c89ece644000a08ec501
ttseel <ojs7928@gmail.com>
2022-10-03 16:04:56 +0900

    Initial commit

---
+++ src/main/java/com/example/dockerstudy/DockerstudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerstudyApplication.class, args);
+	}
+
+}
